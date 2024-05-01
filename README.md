958b022838aa0dba597181962be4e25046f1c859
Thomas Keig <atomatrixdevelopment@gmail.com>
2024-05-02 00:03:18 +0100

    Initial Release

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# thekla-tickets-api
+# thekla-api
+A selfhostable API to show how many tickets are remaining for Thekla Thursday & Saturday club nights.
+## Setup:
+- Clone the repository.
+- Install the requirements with `python -m pip install -r requirements.txt`
+- Edit the settings.yml file to change your server port and more.
+- Run the main.py file on your webserver.
+
+## Endpoint:
+[GET] /remainingTickets
+### Parameters:
+**event_index:** Index from 0 to 1 for the event type. 0 is Pressure, 1 is Pop Confessional
+**date:** The date of the event in the format YYYY-MM-DD (Eg: 2024-05-04)
+**Example URI:** https://example.com/remainingTickets?event_index=1&date=2024-05-04
+### Example Response:
+
+    {"date": "2024-05-04", "event": "pop-confessional", "tickets_remaining": 10}
+**date:** The date of the event
+**event:** The event name
+**tickets_remaining:** How many tickets are left.
+### Limitations:
+Due to how the ticket number is determined, if the tickets remaining is above 10, it is not possible to tell how many tickets are remaining. In this case the ticket count is simply shown as 10.
---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, redirect
+from bs4 import BeautifulSoup
+import requests
+import yaml
+
+
+with open('settings.yml', encoding="utf8") as file:
+    settings = yaml.load(file, Loader=yaml.FullLoader)
+
+
+web = Flask(__name__)
+
+@web.route('/')
+def root():
+    return redirect(settings["root-page-redirect"], code=302)
+
+
+@web.route('/remainingTickets', methods=['GET'])
+async def remainingTickets():
+    event_index = request.args.get('event_index', default='0')
+    date = request.args.get('date', default='2024-05-04')
+
+    if event_index == '1':
+        event = 'pop-confessional-bristol-tickets'
+    else:
+        event = 'pressure-bristol-tickets'
+
+    url = f'https://www.alttickets.com/{event}/bristol-thekla/{date}-21-30'
+    print(url)
+
+    response = requests.get(url)
+
+    # Return an error if AltTickets can't find an event
+    if response.status_code != 200:
+        return 'Error while finding event.', response.status_code
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    select_element = soup.find('select', class_='ticket_qty_selection')  # Find the select element
+
+    # If tickets are available
+    if select_element is not None:
+        options = select_element.find_all('option')  # Get all the options
+        values = [int(option['value']) for option in options]  # Extract the values as integers
+        tickets_available = max(values)
+
+    else:
+        tickets_available = 0
+
+    # Create JSON object
+    json_str = {"event": event.replace('-bristol-tickets', ''), "date": date, "sold_out": (tickets_available==0), "tickets_remaining": tickets_available, "exact_amount_unknown": (tickets_available==10)}
+
+    return json_str, 200
+
+web.run(port=int(settings['port']), host="0.0.0.0")
